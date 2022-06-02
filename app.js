12a7a043a6774e8830154b274ec18367e7132fe8
Leonardo Engler Arruda <leonardo.earruda@gmail.com>
2022-06-02 14:18:22 -0300

    :rocket: padotec landing page!

---
+++ app.js
@@ -0,0 +1,47 @@
+//FADE IN
+
+const appearOptions = {
+    threshhold: 1,
+    rootMargin: '0px 0px -200px 0px',
+}
+const faders = document.querySelectorAll('.fade-in')
+const appearOnScroll = new IntersectionObserver(function(
+        entries,
+        appearOnScroll
+    ) {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) {
+                return
+            } else {
+                entry.target.classList.add('appear')
+                appearOnScroll.unobserve(entry.target)
+            }
+        })
+    },
+    appearOptions)
+
+faders.forEach((fader) => {
+    appearOnScroll.observe(fader)
+})
+
+//HERO SECTION
+
+const menuBar = document.querySelector('.hero__nav')
+const sectionOne = document.querySelector('.hero-container')
+const sectionOneOptions = {}
+
+const sectionOneObserver = new IntersectionObserver(function(
+        entries,
+        sectionOneObserver
+    ) {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) {
+                menuBar.classList.add('nav-scrolled')
+            } else {
+                menuBar.classList.remove('nav-scrolled')
+            }
+        })
+    },
+    sectionOneOptions)
+
+sectionOneObserver.observe(sectionOne)
